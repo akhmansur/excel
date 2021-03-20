37bf630a2330ab1b2d0c69d4d2f7c925107ea552
akhmansur <akhm.mansur@gmail.com>
2021-03-20 22:13:00 +0500

    Finish table resize

--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -5,6 +5,10 @@ export class TableSelection {
     this.current = null
   }
 
+  get selectedIds() {
+    return this.group.map($el => $el.id())
+  }
+
   select($el) {
     this.clear()
     $el.focus().addClass('selected')
@@ -14,11 +18,17 @@ export class TableSelection {
 
   clear() {
     this.group.forEach($el => $el.removeClass(TableSelection.className))
+    this.group = []
   }
 
   selectGroup($group = []) {
     this.clear()
     this.group = $group
     this.group.forEach($el => $el.addClass(TableSelection.className))
+  }
+
+  applyStyle(style) {
+    console.log(this.group)
+    this.group.forEach($el => $el.css(style))
   }
 }
